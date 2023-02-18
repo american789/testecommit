65b74ad33955f42aaf8e3eee093f2d1cdab4a2c6
Gabriel Nunes de Melo <leirbagmelo@yahoo.com.br>
2023-02-18 14:57:39 -0300

    Commit teste

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace ConsoleApp1 {
+    class Program {
+        static void Main(string[] args) {
+            Point P;
+            P.X = 10;
+            P.Y = 15;
+            Console.WriteLine(P);
+            P = new Point();
+            Console.WriteLine(P);
+        }
+    }
+}
